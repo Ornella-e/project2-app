50cada60b1dad1f6b4056065ed605877a01bfd20
EugeBr <eugebordenave@gmail.com>
2022-05-27 19:29:05 +0200

    Profile route and view

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,5 +7,6 @@ router.get("/", (req, res, next) => {
 
 router.use("/", require("./auth.routes"));
 router.use("/", require("./product.routes"));
+router.use("/", require("./profile.routes"));
 
 module.exports = router;
---
+++ routes/profile.routes.js
@@ -0,0 +1,50 @@
+const User = require("../models/User.model");
+
+const router = require("express").Router();
+
+router.get("/profile/:id/edit", async (req, res, next) => {
+    try {
+        const { id } = req.session.currentUser._id;
+        const user = await User.findById(id);
+        res.render("profile/profile-edit", user);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post("/profile/:id/edit", async (req, res, next) => {
+    try {
+        const { id } = req.session.currentUser._id;
+        const { username, password, fullName, email, city, country } = req.body;
+        await User.findByIdAndUpdate(id,
+            {
+                username,
+                password,
+                fullName,
+                email,
+                location: {
+                    city,
+                    country
+                },
+            },
+            {
+                new: true
+            });
+            res.redirect(`/profile/${id}`);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/profile/:id", async (req, res, next) => {
+   // console.log('user id:', req.session.currentUser._id);
+    try {
+        const { id } = req.session.currentUser._id;
+        const user = await User.findById(id)
+        res.render("profile/profile-details", user);
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router;
